38c34d7895895928474562d59cfa55e152ee5c22
Scott Elder <smelder@gmail.com>
2019-05-03 09:48:29 -0500

    Remove create-react-app

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+// import './App.css';
 
 export default class App extends Component {
   constructor(props) {
@@ -22,7 +22,7 @@ export default class App extends Component {
       <>
         <h1>Welcome to Blue Ocean!</h1>
         <ul>
-          { this.state.seaCreatures.map(creature, index => <li key={index}>{creature}</li>) }
+          { this.state.seaCreatures.map((creature, index) => <li key={index}>{creature}</li>) }
         </ul>
       </>
     )
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,18 +1,28 @@
 const path = require('path');
-const SRC_DIR = path.join(__dirname, '/client');
-const DIST_DIR = path.join(__dirname, '/dist');
+const SRC_DIR = path.join(__dirname, '/src');
+const DIST_DIR = path.join(__dirname, '/public');
 module.exports = {
-  entry: `${SRC_DIR}/app.jsx`,
+  entry: `${SRC_DIR}/index.js`,
   output: {
   filename: 'bundle.js',
   path: DIST_DIR
-},
+  },
   module : {
     rules : [
       {
+        test: /\.(js|mjs|jsx)$/,
+        enforce: 'pre',
+        loader: 'eslint-loader'
+      },
+      {
         test : /\.js?/,
         include : SRC_DIR,
-        loader : 'babel-loader',
+        loader : 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        include: SRC_DIR,
+        loader: 'css-loader'
       }
     ]
   }
